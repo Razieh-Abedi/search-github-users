851580dacfbbdd216d472917fb2f0d7dce755e46
Razieh Abedi <raziyeabedi@gmail.com>
2022-11-29 16:45:28 +0800

    created the doughnut chart

--- src/components/charts/Doughnut2d.js
+++ src/components/charts/Doughnut2d.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
 
-function Doughnut2d() {
-  return (
-    <div>Doughnut2d</div>
-  )
-}
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
 
-export default Doughnut2d
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "doughnut2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Stars per Language",
+        theme: "candy",
+        decimals: 0,
+        doughnutRadius: "45%",
+        showPercentValues: 0,
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default ChartComponent;
