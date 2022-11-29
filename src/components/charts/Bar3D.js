a799327cd55220f3a48373704a4e1a494122eebe
Razieh Abedi <raziyeabedi@gmail.com>
2022-11-29 17:43:56 +0800

    crreated the comumns3d and bar3d graphs in the repos

--- src/components/charts/Bar3D.js
+++ src/components/charts/Bar3D.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
 
-function Bar3D() {
-  return (
-    <div>Bar3D</div>
-  )
-}
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
 
-export default Bar3D
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "bar3d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Most Forked",
+        yAxisName: "Forks",
+        xAxisName: "Repos",
+        yAxisNameFontSize: "16px",
+        xAxisNameFontSize: "16px",
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default ChartComponent;
