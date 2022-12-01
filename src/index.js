601cf2bb50b480be71a0130f32d6e4194a5e2c61
Razieh Abedi <raziyeabedi@gmail.com>
2022-12-01 17:15:27 +0800

    did the login authentication

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { GithubProvider } from "./context/context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-la13v2wb5mjftwmn.us.auth0.com"
+      clientId="WJyFBBOxiYoRE1dwuF6DbBa99U90gDzC"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
